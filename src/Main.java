ff6209137c6e5039ba3589b9330465a32af8ba48
G4b1Brandao <anagabriellebrandao2022@gmail.com>
2023-01-27 14:44:09 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import br.com.dio.Desafio.dominio.Curso;
+import br.com.dio.Desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição");
+        curso1.setCargahoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargahoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescrcao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        System.out.println(mentoria);
+
     }
 }
---
+++ src/br/com/dio/Desafio/dominio/Mentoria.java
@@ -0,0 +1,45 @@
+package br.com.dio.Desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    String titulo;
+    String descrcao;
+    LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescrcao() {
+        return descrcao;
+    }
+
+    public void setDescrcao(String descrcao) {
+        this.descrcao = descrcao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descrcao='" + descrcao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
